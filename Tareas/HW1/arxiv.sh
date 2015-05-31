c36ce42536021a6db7fd3858eaa9d32bc74fdb3d
NicolasMorales-Duran <na.morales92@uniandes.edu.co>
2015-05-31 01:44:58 -0500

    Update arxiv.sh

--- Tareas/HW1/arxiv.sh
+++ Tareas/HW1/arxiv.sh
@@ -4,6 +4,7 @@ echo http://arxiv.org/list/cond-mat/new
 echo =====================================
 echo keyword: $1
 echo =====================================
-echo Articles found: curl http://arxiv.org/list/cond-mat/new | grep -iw Title: | sed 's/.*>/+/g' | grep $1 | wc -l
+echo Articles found: 
+curl http://arxiv.org/list/cond-mat/new | grep -iw Title: | sed 's/.*>/+/g' | grep $1 | wc -l
 curl http://arxiv.org/list/cond-mat/new | grep -iw Title: | sed 's/.*>/+/g' | grep $1
 echo =====================================
