b1aada8ca517efa7e1f0c4a66ee0033d7cdbfb6f
NicolasMorales-Duran <na.morales92@uniandes.edu.co>
2015-06-09 23:39:12 -0500

    Update horton1-1.c

--- Tareas/HW2/horton1-1.c
+++ Tareas/HW2/horton1-1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Nicolás Morales-Durán \n"); \\Imprime mi nombre.
-	printf("Calle 86 # 69H-35 Casa 3 \n"); \\Imprime mi dirección.
+	printf("Nicolás Morales-Durán \n"); //Imprime mi nombre.
+	printf("Calle 86 # 69H-35 Casa 3 \n"); //Imprime mi dirección.
 	return 0;
 }
