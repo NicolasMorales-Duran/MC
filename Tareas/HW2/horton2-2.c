0383f496e6be1312ddc03fe530dbedf58d3eb2e1
NicolasMorales-Duran <na.morales92@uniandes.edu.co>
2015-06-09 23:47:17 -0500

    Update horton2-2.c

--- Tareas/HW2/horton2-2.c
+++ Tareas/HW2/horton2-2.c
@@ -10,26 +10,28 @@ int main(void)
 	float width = 0.0f;
 	float area = 0.0f;
 
-	const float inches_per_foot = 12.0f;
-	const float inches_per_yard = 36.0f;
+	const float inches_per_foot = 12.0f; //Define la conversión de pies a pulgadas.
+	const float inches_per_yard = 36.0f; //Define la conversión de yardas a pulgadas.
 
 	printf("Enter length in whole feet: " ) ;
 	scanf("%f", &feet);
 	printf("                                            ...and then inches: " ) ;
-	scanf("%f", &inches);
-	length = feet*inches_per_foot + inches;
-	length /= inches_per_yard;
+	scanf("%f", &inches); //Pide al usuario que introduzca el largo de la habitación.
+	
+	length = feet*inches_per_foot + inches; //Hace la conversión de unidades.
+	length /= inches_per_yard; //Hace la conversión de unidades.
 
 	printf("Enter width in whole feet: " ) ;
 	scanf("%f", &feet);
 	printf("                                            ...and then inches: " ) ;
-	scanf("%f", &inches);
-	width = feet*inches_per_foot + inches;	
-	width /= inches_per_yard;
+	scanf("%f", &inches); //Pide al usuario que introduzca el ancho de la habitación.
+	
+	width = feet*inches_per_foot + inches;	//Hace la conversión de unidades.
+	width /= inches_per_yard; //Hace la conversión de unidades.
 
 	area = length*width;
 
-	printf("Area in squared yards: %.2f\n", area);
+	printf("Area in squared yards: %.2f\n", area); //Imprime el área de la habitación en yardas cuadradas.
 	
 	return 0;
 }
