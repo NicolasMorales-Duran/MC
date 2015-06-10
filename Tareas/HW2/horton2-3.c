ae8dea2bb9a7e5b06c922b98978f9c8e97a147d3
NicolasMorales-Duran <na.morales92@uniandes.edu.co>
2015-06-09 23:49:57 -0500

    Update horton2-3.c

--- Tareas/HW2/horton2-3.c
+++ Tareas/HW2/horton2-3.c
@@ -8,15 +8,15 @@
 		float price = 0.0f;
 		_Bool product = 1;
 
-		const float price_1 = 3.50f;
-		const float price_2 = 5.50f;
+		const float price_1 = 3.50f; //Define los precios de los productos.
+		const float price_2 = 5.50f; //Define los precios de los productos.
 
-		printf("Enter \"1\" for Type 1 product or \"2\" for Type 2 product: ");
+		printf("Enter \"1\" for Type 1 product or \"2\" for Type 2 product: "); //Pide al usuario el producto que desea.
 		scanf("%d", &product_type);
 
 		product = ((product_type==1)?1:0);
 
-		printf("Enter quantity of product: ");
+		printf("Enter quantity of product: "); //Pide al usuario la cantidad.
 		scanf("%d", &quantity);
 
 		if(product==1)
@@ -30,5 +30,5 @@
 			printf("The price is: $ %.2f\n", price);
 		}
 				
-		return 0;
+		return 0; //Calcula el precio de la cantidad de productos que desea el usuario y lo imprime.
 	}
