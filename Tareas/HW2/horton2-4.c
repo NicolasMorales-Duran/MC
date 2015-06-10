9e52ea266ed1d9bd3a36e1ac0dbe40e67d83de97
NicolasMorales-Duran <na.morales92@uniandes.edu.co>
2015-06-09 23:52:02 -0500

    Update horton2-4.c

--- Tareas/HW2/horton2-4.c
+++ Tareas/HW2/horton2-4.c
@@ -8,10 +8,10 @@ int main(void)
 	int dollars;
 	int cents;
 
-	printf("Enter your weekly pay: $");
+	printf("Enter your weekly pay: $"); //Pide el salario semanal del trabajador.
 	scanf("%f", &salary);
 
-	printf("Enter how muc hour you work per week: ");
+	printf("Enter how much hours you work per week: "); //Pide la cantidad de horas que trabaja el usuario.
 	scanf("%f", &hours);
 
 	salary /= hours;
@@ -19,7 +19,7 @@ int main(void)
 	dollars = (int) salary;
 	cents = (int) (salary*100)%100;
 
-	printf("Your average hourly pay rate is %d dollars and %d cents.\n", dollars, cents);			
+	printf("Your average hourly pay rate is %d dollars and %d cents.\n", dollars, cents);	//Imprime el salario promedio por hora.		
 
 	return 0;
 }
