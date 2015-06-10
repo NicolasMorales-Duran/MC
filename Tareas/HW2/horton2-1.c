75e8a75564964561d6c27085aa412a7beb2af2bc
NicolasMorales-Duran <na.morales92@uniandes.edu.co>
2015-06-09 23:42:20 -0500

    Update horton2-1.c

--- Tareas/HW2/horton2-1.c
+++ Tareas/HW2/horton2-1.c
@@ -7,16 +7,16 @@ int main(void)
 	float feet = 0.0f;
 	float yard = 0.0f;
 
-	const float inches_per_foot = 12.0f;
-	const float inches_per_yard = 36.0f;
+	const float inches_per_foot = 12.0f; //Define la conversión de pies a pulgadas. 
+	const float inches_per_yard = 36.0f; //Define la conversión de yardas a pulgadas.
 
 	printf("Enter distance in inches: " ) ;
-	scanf("%f", &inches);
+	scanf("%f", &inches); //Solicita al usuario la distancia a convertir en pulgadas.
 
-	feet = inches/inches_per_foot;
-	yard = inches/inches_per_yard;
+	feet = inches/inches_per_foot; //Hace la conversión de unidades.
+	yard = inches/inches_per_yard; //Hace la conversión de unidades.
 
-	printf("Distance in feet: %.2f\n", feet );
-	printf("Distance in yards: %.2f\n", yard );
+	printf("Distance in feet: %.2f\n", feet ); //Imprime la distancia en pies.
+	printf("Distance in yards: %.2f\n", yard ); //Imprime la distancia en pulgadas.
 	return 0;
 }
